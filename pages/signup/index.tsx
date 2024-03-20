4204ffd2188e21bec7c9e00dc7d5561da9f77249
Carolstolf <carolstolf19@gmail.com>
2024-03-19 23:20:28 -0300

    feat: create stepper

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,10 +1,26 @@
 import NextImage from "next/image";
-import { Anchor, Button, Image, InputBase, Text } from "@mantine/core";
+import {
+  Anchor,
+  Button,
+  Group,
+  Image,
+  InputBase,
+  Stepper,
+  Text,
+} from "@mantine/core";
 import BackButton from "../../public/icons/BackButton.svg";
 import UserIcon from "../../public/icons/UserIcon.svg";
 import { IMaskInput } from "react-imask";
+import { useState } from "react";
+import * as Icons from "../../public/icons/index";
 
 export default function LoginPage() {
+  const [active, setActive] = useState(1);
+
+  const nextStep = () =>
+    setActive((current) => (current < 6 ? current + 1 : current));
+  const prevStep = () =>
+    setActive((current) => (current > 0 ? current - 1 : current));
   return (
     <div className="grid grid-cols-[1.88fr_1fr] h-screen">
       <div className="flex justify-center items-center">
@@ -65,6 +81,132 @@ export default function LoginPage() {
         <Text className="text-[#0F1728] text-center text-[2rem] font-medium leading-[38px] pb-[5rem]">
           Passos para criar sua conta no Lotefy
         </Text>
+        <Stepper
+          active={active}
+          onStepClick={setActive}
+          orientation="vertical"
+          color="#56D963"
+          iconSize={49}
+          allowNextStepsSelect={false}
+          styles={{
+            stepIcon: {
+              backgroundColor: "#D6FFDA",
+              border: "8px solid #D7F5DA",
+            },
+            stepBody: {
+              color: "#0F1728",
+              fontSize: "16px",
+              fontWeight: 500,
+              lineHeight: "24px",
+              fontFamily: "DM Sans",
+
+              // "&[data-active]": {
+              //   backgroundColor: "red",
+              //   color: "red",
+              // },
+            },
+          }}
+          completedIcon={
+            <Image
+              component={NextImage}
+              src={Icons.CompletedIcon}
+              alt="Logo"
+              h={37}
+              w={37}
+              style={{
+                backgroundColor: "#56D963",
+                padding: "7px",
+                borderRadius: "28px",
+              }}
+            />
+          }
+        >
+          <Stepper.Step
+            label="Identificação"
+            description="Usamos seu CPF para te identificar"
+            icon={
+              <Image
+                component={NextImage}
+                src={Icons.UserIcon}
+                alt="Logo"
+                h={20}
+                w={20}
+              />
+            }
+          />
+          <Stepper.Step
+            label="Dados pessoais"
+            description="Digite seu nome e data de nascimento"
+            icon={
+              <Image
+                component={NextImage}
+                src={Icons.UserIconCheck}
+                alt="Logo"
+                h={20}
+                w={20}
+              />
+            }
+          />
+          <Stepper.Step
+            label="Número de telefone e e-mail"
+            description="Utilizamos o seu número de telefone e conta de e-mail para validar o seu acesso"
+            icon={
+              <Image
+                component={NextImage}
+                src={Icons.MessageIcon}
+                alt="Logo"
+                h={20}
+                w={20}
+              />
+            }
+          />
+          <Stepper.Step
+            label="Dados da empresa"
+            description="Utilizamos esses dados para entender o momento da sua loteadora"
+            icon={
+              <Image
+                component={NextImage}
+                src={Icons.BuildingIcon}
+                alt="Logo"
+                h={20}
+                w={20}
+              />
+            }
+          />
+          <Stepper.Step
+            label="Crie sua senha"
+            description="Crie sua senha para acessar a plataforma"
+            icon={
+              <Image
+                component={NextImage}
+                src={Icons.PasscodeIcon}
+                alt="Logo"
+                h={20}
+                w={20}
+              />
+            }
+          />
+          <Stepper.Step
+            label="Termos de uso"
+            description="Leia e aceite os termos de uso"
+            icon={
+              <Image
+                component={NextImage}
+                src={Icons.FileIcon}
+                alt="Logo"
+                h={20}
+                w={20}
+              />
+            }
+          />
+          <Stepper.Completed> </Stepper.Completed>
+        </Stepper>
+        <Group justify="center" mt="xl">
+          <Button variant="default" onClick={prevStep}>
+            Back
+          </Button>
+          <Button onClick={nextStep}>Next step</Button>
+        </Group>
       </div>
     </div>
   );
---
+++ public/icons/index.ts
@@ -0,0 +1,7 @@
+export * as UserIcon from "./user.svg";
+export * as UserIconCheck from "./user-check.svg";
+export * as MessageIcon from "./message-notification-square.svg";
+export * as BuildingIcon from "./tabler_building.svg";
+export * as PasscodeIcon from "./passcode.svg";
+export * as FileIcon from "./file.svg";
+export * as CompletedIcon from "./check-circle-broken.svg";
