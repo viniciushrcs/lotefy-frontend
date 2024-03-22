b4301e556d39158f5720a85cb9afa8c93df76414
Carolstolf <carolstolf19@gmail.com>
2024-03-21 23:17:53 -0300

    feat: create signup context and fix font import

---
+++ context/SignUpContext.tsx
@@ -0,0 +1,37 @@
+import { createContext, useState, useContext, useMemo } from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+interface SignUpStore {
+  userData: Record<string, string | undefined>;
+  updateUserData: (_newUserData: Record<string, string>) => void;
+}
+
+export const SignUpContext = createContext<SignUpStore>({
+  userData: {},
+  updateUserData: () => {},
+});
+
+export const SignUpProvider = ({ children }: Props) => {
+  const [userData, setUserData] = useState<Record<string, string>>({});
+
+  const updateUserData = (newData: Record<string, string>) => {
+    setUserData(newData);
+  };
+
+  const contextValue = useMemo<SignUpStore>(
+    () => ({
+      userData,
+      updateUserData,
+    }),
+    [userData, updateUserData]
+  );
+
+  return (
+    <SignUpContext.Provider value={contextValue}>
+      {children}
+    </SignUpContext.Provider>
+  );
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,34 +3,22 @@ import Head from "next/head";
 import { MantineProvider } from "@mantine/core";
 import { theme } from "../theme";
 import "./../styles/globals.css";
+import { SignUpProvider } from "../context/SignUpContext";
 
 export default function App({ Component, pageProps }: any) {
   return (
-    <MantineProvider theme={theme}>
-      <Head>
-        <title>Lotefy</title>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
-        />
-        {/* <link rel="shortcut icon" href="/favicon.svg" /> */}
-        <link
-          rel="preconnect"
-          href="https://fonts.googleapis.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap"
-          rel="stylesheet"
-          crossOrigin="anonymous"
-        />
-      </Head>
-      <Component {...pageProps} />
-    </MantineProvider>
+    <SignUpProvider>
+      <MantineProvider theme={theme}>
+        <Head>
+          <title>Lotefy</title>
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+          />
+          {/* <link rel="shortcut icon" href="/favicon.svg" /> */}
+        </Head>
+        <Component {...pageProps} />
+      </MantineProvider>
+    </SignUpProvider>
   );
 }
