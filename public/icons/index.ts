6822238e2272aef7f0f89e4b4bfac5cc7abd4bbb
Carolstolf <carolstolf19@gmail.com>
2024-03-24 16:32:40 -0300

    feat: add icons

--- public/icons/index.ts
+++ public/icons/index.ts
@@ -12,3 +12,11 @@ export * as MessageRegisterIcon from "./MessageIcon.svg";
 export * as CompanyIcon from "./CompanyIcon.svg";
 export * as FileRegisterIcon from "./FileIcon.svg";
 export * as PasswordIcon from "./PasswordIcon.svg";
+
+export * as ArrowRight from "./arrow-right.svg";
+export * as Phone from "./phone.svg";
+export * as Email from "./mail.svg";
+export * as CheckIcon from "./check-icon.svg";
+export * as CheckIconGreen from "./check-icon-green.svg";
+export * as CheckIconRed from "./check-icon-red.svg";
+export * as BrazilFlag from "./brazil-flag.svg";
