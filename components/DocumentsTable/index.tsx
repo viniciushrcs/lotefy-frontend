8421af187f0a4ae877bab61a30d55288142f83e4
Alice Arruda Atalla <aliceatalla36@gmail.com>
2024-08-29 16:18:09 -0300

    fix: adjust lint

--- components/DocumentsTable/index.tsx
+++ components/DocumentsTable/index.tsx
@@ -6,37 +6,45 @@ export function DocumentsTable() {
   const dateMock = formatDate('2023-10-02');
 
   const elements = [
-    { name: 'Nome do documento', date: dateMock, category: 'Projeto e Aprovação',href: "" },
     {
       name: 'Nome do documento',
       date: dateMock,
-      category: 'Documento do empreendimento',href: ""
+      category: 'Projeto e Aprovação',
+      href: '',
+    },
+    {
+      name: 'Nome do documento',
+      date: dateMock,
+      category: 'Documento do empreendimento',
+      href: '',
     },
     { name: 'Nome do documento', date: dateMock, category: 'SPE e SCP' },
-    { name: 'Nome do documento', date: dateMock, category: 'SPE e SCP',href: "" },
+    { name: 'Nome do documento', date: dateMock, category: 'SPE e SCP', href: '' },
     {
       name: 'Nome do documento',
       date: dateMock,
-      category: 'Documento do empreendimento',href: ""
+      category: 'Documento do empreendimento',
+      href: '',
     },
   ];
 
   const rows = elements.map((element, index) => (
     <Table.Tr key={`${element.name}${index}`}>
-      <Table.Td className='font-semibold'>{element.name}</Table.Td>
+      <Table.Td className="font-semibold">{element.name}</Table.Td>
       <Table.Td>{element.date}</Table.Td>
       <Table.Td>{element.category}</Table.Td>
       <Table.Td>
         <div className="flex">
           <a
-          href={element.href}
-             target="_blank" rel="noopener noreferrer"
+            href={element.href}
+            target="_blank"
+            rel="noopener noreferrer"
             className="flex justify-center items-center min-w-8 min-h-8 bg-[#F6F6F6] rounded-xl mr-3"
           >
             <IconEye size={20} color="#737577" />
           </a>
           <a
-          href={element.href}
+            href={element.href}
             download="document.pdf"
             className="flex justify-center items-center min-w-8 min-h-8 bg-[#F6F6F6] rounded-xl"
           >
